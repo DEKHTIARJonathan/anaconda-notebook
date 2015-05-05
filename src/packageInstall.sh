352282032de3a39f3ef99c5a39325c610fe815c7
Jonathan DEKHTIAR <contact@jonathandekhtiar.eu>
2015-05-05 14:19:03 +0200

    Env Vars fixed

--- src/packageInstall.sh
+++ src/packageInstall.sh
@@ -1,3 +1,21 @@
+# Constants
+HOME=/home/condauser
+BASH_RC=/home/condauser/.bashrc
+PREFIX=/home/condauser/anaconda3
+
+PY3PATH=$PREFIX/bin
+PY2PATH=$PREFIX/envs/python2/bin
+
+# python 3
+CONDA3=$PY3PATH/conda
+PIP3=$PY3PATH/pip
+
+# python 2
+CONDA2=$PY2PATH/conda
+PIP2=$PY2PATH/pip
+
+cd $HOME
+
 #conda package install - Python 3
 export PATH=~/anaconda3/bin:$PATH
 conda update --yes --all
